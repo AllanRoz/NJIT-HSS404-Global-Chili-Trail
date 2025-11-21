e50110cfc081ba16cfdceafece5c3005a35bb3b1
AllanRoz <114365300+AllanRoz@users.noreply.github.com>
2025-11-21 16:35:07 -0500

    Github Pages Deployment

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NJIT-HSS404-Global-Chili-Trail/'
+  base: '/NJIT-HSS404-Global-Chili-Trail'
 })
